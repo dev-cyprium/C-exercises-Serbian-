0b158071b8b0748ca49e8f1b344d805784772fa8
Stefan Kupresak <stefan_vg@hotmail.com>
2017-01-19 15:10:28 +0100

    Add solution to zadatak8

--- zadatak8.c
+++ zadatak8.c
@@ -5,5 +5,18 @@
 #include <stdio.h>
 
 int main() {
-	
+	// Varijable koje koristi program
+	int i;
+	int t; // Broj kada se izbaci srednja cifra
+
+	// Petlja koja ide kroz sve trocifrene brojeve
+	for(i = 100; i < 1000; i++) {
+		t = ((i / 100) * 10) + (i % 10); // Matematicko dobijanje trazenog broja
+		// Ispisati brojeve koji su deljivi sa ovim dobijenim brojem
+		if(i % t == 0) {
+			printf("Broj: %d\n", i);
+		}
+	}
+
+	return 0;
 }
