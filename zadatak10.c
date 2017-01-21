919fa0fd81579365cf1e3288948be5db5c0cc8a5
Stefan Kupresak <stefan_vg@hotmail.com>
2017-01-21 12:10:14 +0100

    Add text to zadatak10.c

--- zadatak10.c
+++ zadatak10.c
@@ -1,3 +1,17 @@
 /*
-   Jos nije gotovo
-*/
+ 	10. Obim prednjeg točka traktora je Ncm, a zadnjeg Mcm.
+  Odrediti najmanju dužinu puta koji traktor treba da pređe, tako da oba točka naprave ceo broj obrtaja. 	 
+ */
+
+int main() {
+	// Varijable
+	int n, m;
+
+	printf("Unesite duzinu prednjeg tocka u cm: ");
+	scanf("%d", &n);
+
+	printf("Unesite duzinu zadnjeg tocka u cm: ");
+	scanf("%d", &m);
+
+	/* Ne znam */
+}
